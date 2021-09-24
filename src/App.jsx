ef2213963c57ae03d62f70e483e3bb401bd6936c
vinaka <61833409+vinaka-log@users.noreply.github.com>
2021-09-24 11:05:51 +0900

    useState & useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const OnClickButton = () => alert();
-  const contentStyle = {
-    color: "red",
-    fontSize: "24px"
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShow, setFaceShow] = useState(true);
+
+  const OnClickCountUp = () => setNum(num + 1);
+
+  const onclickSwitchShow = () => {
+    setFaceShow(!faceShow);
   };
+
+  useEffect(() => {
+    console.log("useEffect");
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShow || setFaceShow(true);
+      } else {
+        faceShow && setFaceShow(false);
+      }
+    }
+  }, [num]);
 
   return (
     <>
       <h1 style={{ color: "red" }}>コンにちは</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">電気</ColorfulMessage>
-      <button onClick={OnClickButton}>ボタン</button>
+      <button onClick={OnClickCountUp}>カウントアップ</button>
+      <button onClick={onclickSwitchShow}>on/off</button>
+      <p>{num}</p>
+      {faceShow && <p>🔘</p>}
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 const ColorfulMessage = (props) => {
   const { color, children } = props;
-  console.log(props);
   const contentStyle = {
     color,
     fontSize: "18px"
